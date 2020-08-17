8b4cd3067cc3607489b5b01732e89470e40571d8
brochan <brochacho.the.newfag@gmail.com>
2020-08-17 22:55:58 +0100

    added unban command

--- bot.py
+++ bot.py
@@ -82,5 +82,21 @@ async def kick(ctx, member : discord.Member, *, reason=None):
 @client.command()
 async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.mention}')
+#ban command
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+#unban command
 
 client.run('NzQ0OTE0OTc4NDY0ODU4MjA0.XzqKIg.mOwN3yyJZCmkRCxMBb3-0wyxf6U')
