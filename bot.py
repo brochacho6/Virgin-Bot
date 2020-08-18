f3c4a958372674257fbf16260b71d41fa91896a6
brochan <brochacho.the.newfag@gmail.com>
2020-08-18 12:36:51 +0100

    rewrote the bot

--- bot.py
+++ bot.py
@@ -1,28 +1,48 @@
-import random
-
+from random import random
 import discord
+import os
 from discord.ext import commands
+
 client = commands.Bot(command_prefix='$')
+
 
 @client.event
 async def on_ready():
     print('Bot is Online!')
-#online message
 
 @client.event
 async def on_member_join(member):
-    print(f'{member} has joined the server!')
-#join message
+    await member.send("Welcome to the Server!")
 
 @client.event
 async def on_member_remove(member):
-    print(f'{member} has left the server!')
-#leaving message
+    await member.send('Bye!')
+
+
+# online message, only terminal side
+
+@client.command(aliases=['nuke', 'purge'])
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount: int):
+    await ctx.channel.purge(limit=amount + 1)
+
+
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please specify an ammount of messages to delete.')
+
+
+@client.event
+async def on_command_error(ctx, error):
+    pass
+# chat purge command
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! 🏓 {round(client.latency * 1000)}ms')
 #ping command
+
 
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
@@ -65,38 +85,20 @@ async def _8ball(ctx, *, question):
 "Outlook not so good.",
 
 "Very doubtful."]
-
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 #8ball command
 
-@client.command(aliases=['nuke', 'purge'])
-async def clear(ctx, amount=20):
-    await ctx.channel.purge(limit=amount + 1)
-#chat purge command
-
 @client.command()
-async def kick(ctx, member : discord.Member, *, reason=None):
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
     await member.kick(reason=reason)
 #kick command
 
 @client.command()
+@commands.has_permissions(ban_members=True)
 async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
     await ctx.send(f'Banned {member.mention}')
 #ban command
-
-@client.command()
-async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('#')
-
-    for ban_entry in banned_users:
-        user = ban_entry.user
-
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(f'Unbanned {user.mention}')
-            return
-#unban command
 
 client.run('NzQ0OTE0OTc4NDY0ODU4MjA0.XzqKIg.mOwN3yyJZCmkRCxMBb3-0wyxf6U')
