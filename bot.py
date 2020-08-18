300526929c31e39f56fa4756c5b98548e1887198
brochan <brochacho.the.newfag@gmail.com>
2020-08-18 21:43:59 +0100

    added pvg command

--- bot.py
+++ bot.py
@@ -55,6 +55,9 @@ async def ping(ctx):
 async def brownpill(ctx):
     await ctx.send(f"*blows vape cloud* smoke weed every day! are you playing constantiam bro? major cringe. yeah i joined 2b2t in 2012. *dabs* ew, youre a rusher? brownpill alert. *flips hair* yeah, ive heard of fitmc, but have you heard of pekee of 2b2t? *sneers in disgust* no? bluepill alert. well, i guess ill see you on the oldest anarchy server in minecraft history!")
 
+@client.command(brief = "clout")
+async def clout(ctx):
+    await ctx.send(f"DO YOU HAVE ANY IDEA OF HOW MUCH CLOUT I HAVE? HOW MUCH RELEVANCE FLOWS THROUGH MY VETERAN VEINS? YOU CANT POSSIBLY DENY ME!")
 
 
 @client.command(brief = "Checks the Bot's statistics")
@@ -77,8 +80,23 @@ async def logout_error(ctx, error):
         await ctx.send("Hey, you cant do that >:(")
 
 
-@client.command(aliases=['8ball'], brief='Ask the 8ball a question!',
-                description='Ask the 8ball anything and it will randomize 1 out of 20 possible responses.')
+@client.command()
+async def pvg(ctx):
+    virginity = ["https://cdn.discordapp.com/emojis/736080066621997218.png?v=1",
+                "https://cdn.discordapp.com/emojis/717922779659501659.png?v=1",
+                "https://cdn.discordapp.com/emojis/737935384297734194.png?v=1",
+                "https://cdn.discordapp.com/emojis/698308907910562001.png?v=1",
+                "https://cdn.discordapp.com/emojis/686212562022301734.png?v=1",
+                "https://cdn.discordapp.com/emojis/697939921557782579.png?v=1",
+                "https://cdn.discordapp.com/emojis/737935217255514193.png?v=1",
+                "https://cdn.discordapp.com/emojis/738510553428787262.png?v=1",
+                "https://cdn.discordapp.com/emojis/737935704642027521.png?v=1",
+                "https://cdn.discordapp.com/emojis/745150242558574743.png?v=1",
+                "https://cdn.discordapp.com/emojis/745150242558574743.png?v=1",]
+    await ctx.send(f"{random.choice(virginity)}")
+
+
+@client.command(aliases=['8ball'], brief='Ask the 8ball a question!',description='Ask the 8ball anything and it will randomize 1 out of 20 possible responses.')
 async def _8ball(ctx, *, question):
     responses = ["It is certain.",
 
@@ -139,6 +157,5 @@ async def ban(ctx, member: discord.Member, *, reason=None):
     await ctx.send(f'Banned {member.mention}')
 
 
-# ban command
 
 client.run('NzQ1MzIyOTM4MTAxNzkyOTAx.XzwGEw.xKauE9JYoJpvObXOUKD4rjJBJzk')
