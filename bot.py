c9f7ed192dcd666125c617d89e453ff8fb6b40f4
brochan <brochacho.the.newfag@gmail.com>
2020-08-18 20:34:48 +0100

    changed shit

--- bot.py
+++ bot.py
@@ -1,27 +1,33 @@
-from random import random
+import random
+import platform
 import discord
+import json
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='$')
+client = commands.Bot(command_prefix='$', case_insensitive=True)
 
 
 @client.event
 async def on_ready():
     print('Bot is Online!')
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game(name='Virgin and Proud!'))
+
+
+@client.command(brief = 'Echoes a message (admin only)')
+@commands.has_permissions(administrator=True)
+async def echo(ctx, *, message=None):
+    message = message or "Please provide message to be echo'd"
+    await ctx.message.delete()
+    await ctx.send(message)
+
 
 @client.event
 async def on_member_join(member):
     await member.send("Welcome to the Server!")
 
-@client.event
-async def on_member_remove(member):
-    await member.send('Bye!')
 
-
-# online message, only terminal side
-
-@client.command(aliases=['nuke', 'purge'])
+@client.command(aliases=['nuke', 'purge'], brief='Clears a certain ammount of messages (admin only)')
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount: int):
     await ctx.channel.purge(limit=amount + 1)
@@ -30,75 +36,94 @@ async def clear(ctx, amount: int):
 @clear.error
 async def clear_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Please specify an ammount of messages to delete.')
+        await ctx.send('Please specify an amount of messages to delete.')
 
 
 @client.event
 async def on_command_error(ctx, error):
     pass
+
+
 # chat purge command
 
-@client.command()
+@client.command(brief="Checks the Bot's Ping", description="Checks the Bot's Ping")
 async def ping(ctx):
     await ctx.send(f'Pong! 🏓 {round(client.latency * 1000)}ms')
-#ping command
 
 
-@client.command(aliases=['8ball'])
+# ping command
+
+@client.command(brief = "Checks the Bot's statistics")
+async def botstats(ctx):
+    pythonVersion = platform.python.version()
+    dpyVersion = discord.__version__
+    serverCount = len(client.guilds)
+    memberCount = len(set(client.get_all_members()))
+    await ctx.send(f"Bot Stats:\nI'm in {serverCount} servers with a total of {memberCount} members. :sunglasses:\nI'm running Python {pythonVersion} and discord.py {dpyVersion}.")
+
+
+@client.command(aliases=['8ball'], brief='Ask the 8ball a question!',
+                description='Ask the 8ball anything and it will randomize 1 out of 20 possible responses.')
 async def _8ball(ctx, *, question):
     responses = ["It is certain.",
 
-"It is decidedly so.",
+                 "It is decidedly so.",
 
-"Without a doubt.",
+                 "Without a doubt.",
 
-"Yes - definitely.",
+                 "Yes - definitely.",
 
-"You may rely on it.",
+                 "You may rely on it.",
 
-"As I see it, yes.",
+                 "As I see it, yes.",
 
-"Most likely.",
+                 "Most likely.",
 
-"Outlook good.",
+                 "Outlook good.",
 
-"Yes.",
+                 "Yes.",
 
-"Signs point to yes.",
+                 "Signs point to yes.",
 
-"Reply hazy, try again.",
+                 "Reply hazy, try again.",
 
-"Ask again later.",
+                 "Ask again later.",
 
-"Better not tell you now.",
+                 "Better not tell you now.",
 
-"Cannot predict now.",
+                 "Cannot predict now.",
 
-"Concentrate and ask again.",
+                 "Concentrate and ask again.",
 
-"Don't count on it.",
+                 "Don't count on it.",
 
-"My reply is no.",
+                 "My reply is no.",
 
-"My sources say no.",
+                 "My sources say no.",
 
-"Outlook not so good.",
+                 "Outlook not so good.",
 
-"Very doubtful."]
+                 "Very doubtful."]
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
-#8ball command
 
-@client.command()
+
+# 8ball command
+
+@client.command(brief='Kicks a member that is mentioned')
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
     await member.kick(reason=reason)
-#kick command
 
-@client.command()
+
+# kick command
+
+@client.command(brief='Bans the user that is mentioned', category='Moderation')
 @commands.has_permissions(ban_members=True)
-async def ban(ctx, member : discord.Member, *, reason=None):
+async def ban(ctx, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
     await ctx.send(f'Banned {member.mention}')
-#ban command
 
-client.run('NzQ0OTE0OTc4NDY0ODU4MjA0.XzqKIg.mOwN3yyJZCmkRCxMBb3-0wyxf6U')
+
+# ban command
+
+client.run('NzQ1MzIyOTM4MTAxNzkyOTAx.XzwGEw.xKauE9JYoJpvObXOUKD4rjJBJzk')
