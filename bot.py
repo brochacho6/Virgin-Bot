118e405654fd0bc0852f19ca6ed966c9fb0af717
brochan <brochacho.the.newfag@gmail.com>
2020-08-20 03:27:22 +0100

    l

--- bot.py
+++ bot.py
@@ -54,53 +54,6 @@ async def stats(ctx):
         f"Bot Stats:\nI'm in {serverCount} servers with a total of {memberCount} members. :sunglasses:\nI'm running Python {pythonVersion} and discord.py {dpyVersion}.")
 
 
-@client.command(aliases=['8ball'])
-async def _8ball(ctx, *, question):
-    responses = ["It is certain.",
-
-                 "It is decidedly so.",
-
-                 "Without a doubt.",
-
-                 "Yes - definitely.",
-
-                 "You may rely on it.",
-
-                 "As I see it, yes.",
-
-                 "Most likely.",
-
-                 "Outlook good.",
-
-                 "Yes.",
-
-                 "Signs point to yes.",
-
-                 "Reply hazy, try again.",
-
-                 "Ask again later.",
-
-                 "Better not tell you now.",
-
-                 "Cannot predict now.",
-
-                 "Concentrate and ask again.",
-
-                 "Don't count on it.",
-
-                 "My reply is no.",
-
-                 "My sources say no.",
-
-                 "Outlook not so good.",
-
-                 "Very doubtful."]
-    _8ballEmbed = discord.Embed(
-        colour=discord.Colour.red()
-    )
-    _8ballEmbed.set_author(name=f'Question: {question}\nAnswer: {random.choice(responses)}')
-    await ctx.send(embed=_8ballEmbed)
-
 
 # 8ball command
 
